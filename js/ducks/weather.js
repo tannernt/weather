4d1e8f6e407c3d41a1fe5bea8309afc22315792c
Nathan Tanner <Tanner@Nathans-MBP-2.hsd1.co.comcast.net>
2017-02-04 10:33:47 -0700

    Added initial weather api url and credentials.

--- js/ducks/weather.js
+++ js/ducks/weather.js
@@ -1,4 +1,27 @@
+import Request from 'superagent';
+
 export const GET_CITY_FORECAST_SUCCESS = 'GET_CITY_FORECAST_SUCCESS';
+export const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/';
+export const WEATHER_API_KEY = '5c235b4f603679abafdad752cb2e669b';
+export const getCityForecast = (city) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch({
+        type: GET_CITY_FORECAST_SUCCESS,
+        payload: {
+          name: city,
+          forecast: [
+            { temp: 1 },
+            { temp: 2 },
+            { temp: 3 },
+            { temp: 4 },
+          ],
+        },
+      });
+    }, 2000);
+  };
+};
+
 export const getCityForecast = (city) => {
   return (dispatch) => {
     setTimeout(() => {
